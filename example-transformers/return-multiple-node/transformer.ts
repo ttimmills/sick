f5055acbfc4b955135c745fbf1caab817af9f43b
ttimmills <testtechmmills@yahoo.com>
2025-03-26 01:14:16 +0000

    Add UI - enhancing logs

--- example-transformers/return-multiple-node/transformer.ts
+++ example-transformers/return-multiple-node/transformer.ts
@@ -1,8 +1,8 @@
 import * as ts from 'typescript';
 
-const transformer: ts.TransformerFactory<ts.SourceFile> = context => {
+let transformer: ts.TransformerFactory<ts.SourceFile> = context => {
   return sourceFile => {
-    const visitor = (node: ts.Node): ts.VisitResult<ts.Node> => {
+    let visitor = (node: ts.Node): ts.VisitResult<ts.Node> => {
       // If it is a expression statement,
       if (ts.isExpressionStatement(node)) {
         // Return it twice.
