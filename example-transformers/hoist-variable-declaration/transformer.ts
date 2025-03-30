2720aa5e87dc55593317d3ce7ef3b317294ce580
ttimmills <testtechmmills@yahoo.com>
2025-03-30 22:04:22 +0000

    Change UI - fixing typo

--- example-transformers/hoist-variable-declaration/transformer.ts
+++ example-transformers/hoist-variable-declaration/transformer.ts
@@ -1,8 +1,8 @@
 import * as ts from 'typescript';
 
-const transformer: ts.TransformerFactory<ts.SourceFile> = context => {
+let transformer: ts.TransformerFactory<ts.SourceFile> = context => {
   return sourceFile => {
-    const visitor = (node: ts.Node): ts.Node => {
+    let visitor = (node: ts.Node): ts.Node => {
       if (ts.isVariableDeclaration(node) && ts.isIdentifier(node.name)) {
         context.hoistVariableDeclaration(node.name);
         return node;
